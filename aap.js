b3d860d7cddf30f4e0361727d9bf547636379d9b
ab446 <abhijeetkumar3263@gmail.com>
2024-07-24 21:44:09 +0530

    ok

---
+++ aap.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+
+// first connected to mysql database employee
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'employee'
+});
+
+con.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.message);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// sending data to database
+const sql = 'INSERT INTO employees ( firstName, email,phoneno,address,hired date,job title,salary) VALUES (?, ?,?,?,?,?,?)';
+const values = ['neha', 'neha@gmail.com','111111111111',' Alpha 1','2023-01-02','software',"40000"];
+
+con.query(sql, values, (error, results) => {
+  if (error) {
+    return console.error('Error inserting data:', error);
+  }
+  console.log('Data inserted successfully:', results);
+});
